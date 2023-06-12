01719e0c28d598dace1cc98253581cb1074b7a6f
Noufal Vattaparambil <noufalvp9809@gmail.com>
2023-06-12 20:29:33 +0400

    first commit

---
+++ pro.js
@@ -0,0 +1,35 @@
+//1
+let n=10,i=1;
+for (let i=1;i<=10;i++){
+    result=i*n
+console.log(i,"x",n,'=',result);
+}
+
+//2
+//var a=[1,3,9,12,15,18,21];
+//for (var a=0;i<7;a++){
+//    sq=a[a]**2
+//    console.log(sq);
+//}
+ //3
+ let mark=35;
+if (mark>=90 && mark<=100){
+    console.log('A');
+}else if
+    (mark>=80 && mark<90){
+        console.log('B');
+}else if
+    (mark>=70 && mark<80){
+        console.log('C');
+}else if
+    (mark>=60 && mark<70){
+        console.log('D');
+}else if
+    (mark>=50 && mark<60){
+    console.log('E');
+}else if
+    (mark<50){
+        console.log('Failed')
+}else{
+    console.log('Mark')
+}
